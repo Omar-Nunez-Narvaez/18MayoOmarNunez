34729c7bd97a5f23601b72ec7233f190ace5162e
Omar-Nunez-Narvaez <omar.nunez@udla.edu.ec>
2026-05-18 14:18:07 -0500

    Segundo commit agregando puntero Omar Nuñez

--- ejercicio1.c
+++ ejercicio1.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
 
-int suma(int a, int b) {
-    return a + b;
-}
+int suma(int a, int b);
+int  sumaxref(int *ptra, int *ptrb);
 
 int main (){
 
@@ -12,6 +11,14 @@ int main (){
     scanf("%d", &a);
     printf("Ingrese el segundo numero: ");
     scanf("%d", &b);
-
+    printf("El resultado es: %d\n", suma(a,b));
+    printf("El resultado es: %d", sumaxref(&a,&b));
+    return 0;
 
 }
+ int suma(int a, int b){
+    return a+b;
+ }
+ int sumaxref (int *ptra, int *ptrb){
+    return *ptra + *ptrb;
+ }
