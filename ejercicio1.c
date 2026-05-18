dfc83db6c91c2e56a44e3f694fccd6beb7bae60a
Omar-Nunez-Narvaez <omar.nunez@udla.edu.ec>
2026-05-18 14:22:55 -0500

    Tercer commit Agragando resta Omar Nunez

--- ejercicio1.c
+++ ejercicio1.c
@@ -2,23 +2,41 @@
 
 
 int suma(int a, int b);
-int  sumaxref(int *ptra, int *ptrb);
+int sumaxref(int *ptra, int *ptrb);
 
-int main (){
 
-    int a,b;
+int resta(int a, int b);
+int restaxref(int *ptra, int *ptrb);
+
+int main() {
+    int a, b;
+
     printf("Ingrese el primer numero: ");
     scanf("%d", &a);
     printf("Ingrese el segundo numero: ");
     scanf("%d", &b);
-    printf("El resultado es: %d\n", suma(a,b));
-    printf("El resultado es: %d", sumaxref(&a,&b));
-    return 0;
+    printf("El resultado de la suma es: %d\n", suma(a, b));
+    printf("El resultado de la suma es: %d\n", sumaxref(&a, &b));
 
+    printf("El resultado de la resta es: %d\n", resta(a, b));
+    printf("El resultado de la resta es: %d\n", restaxref(&a, &b));
+
+    return 0;
 }
- int suma(int a, int b){
-    return a+b;
- }
- int sumaxref (int *ptra, int *ptrb){
+
+
+int suma(int a, int b) {
+    return a + b;
+}
+
+int sumaxref(int *ptra, int *ptrb) {
     return *ptra + *ptrb;
- }
+}
+
+int resta(int a, int b) {
+    return a - b;
+}
+
+int restaxref(int *ptra, int *ptrb) {
+    return *ptra - *ptrb;
+}
