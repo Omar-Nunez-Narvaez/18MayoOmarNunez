66042d5d6903c844c1c855ee555930f10d9c99c9
Omar-Nunez-Narvaez <omar.nunez@udla.edu.ec>
2026-05-18 14:05:13 -0500

    Subiendo el ejercicio Omar Nunez

---
+++ ejercicio1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+int suma(int a, int b) {
+    return a + b;
+}
+
+int main (){
+
+    int a,b;
+    printf("Ingrese el primer numero: ");
+    scanf("%d", &a);
+    printf("Ingrese el segundo numero: ");
+    scanf("%d", &b);
+
+
+}
